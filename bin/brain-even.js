2a0461faa1258cbc5c15b9b27ae467d6c5e0b586
Даниил <wsx86932321@yandex.ru>
2023-11-27 10:01:32 +0300

    step-5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,8 +7,7 @@ import { getName } from "../src/cli.js";
 export function evenness() {
     console.log('Welcome to the Brain Games!');
     var userName = getName()
-    console.log(`Hello, ${userName}!`);
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    console.log(`Hello, ${userName}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
     for (let i = 0; i < 3; i += 1) {
         var num = Math.round(Math.random() * 100)
         console.log(`Question: ${num}`);
@@ -20,7 +19,7 @@ export function evenness() {
                 console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`)
                 i = 3
             }
-        } else if (num % 2 !== 0) {
+        } else {
             if (answer === 'no') {
                 console.log('Correct!')
             } else {
